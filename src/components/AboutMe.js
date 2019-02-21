fd44eb490454ee765f2a5bfb09b2ba694692f87d
nickJ <nicknotjustin@gmail.com>
2019-02-21 17:51:30 -0600

    Started on AboutMe page. Two containers (one for the whole thing, one for the picture and words.

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,11 +1,21 @@
 import React, {Component} from 'react';
+import './AboutMe.css'
 
 export default class AboutMe extends Component{
 
+    returnImg(){
+        const img = require('../images/Profile_Pic.jpg');
+        return img;
+    }
+
     render(){
         return (
-            <div role='container'>
-                <h1>'Sup?</h1>
+            <div role='container' className='aboutMeContainer'>
+              <h1>Who am I?</h1>
+              <div role='container' className='aboutMeImg'>
+                <p>This is going to be a bunch of really funny, but smart stuff</p>
+                <img className='revolvingImage'alt='this is temporary' src={this.returnImg()} />
+              </div>
             </div>
         )
     }
